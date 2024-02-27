4bd872366ac5b8343aaa3fd2887e98e0661dd541
Jayesh A <111209763+ThatCyberNerd@users.noreply.github.com>
2024-02-27 19:47:20 +0530

    Create weathersms.py

---
+++ Day 35 : Open Weather Map SMS/weathersms.py
@@ -0,0 +1,34 @@
+import requests
+from twilio.rest import Client
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "__YOUR_OWM_API_KEY__"
+account_sid = "__YOUR_TWILIO_ACCOUNT_ID__"
+auth_token = "__YOUR_TWILIO_AUTH_TOKEN__"
+
+weather_params = {
+    "lat": 46.947975,
+    "lon": 7.447447,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
