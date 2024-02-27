9b6811c7df0c6caeff73eb8824c81c4dabb201a3
Jayesh A <111209763+ThatCyberNerd@users.noreply.github.com>
2024-02-27 19:57:29 +0530

    Update weathersms.py

--- Day 35 : Open Weather Map SMS/weathersms.py
+++ Day 35 : Open Weather Map SMS/weathersms.py
@@ -2,7 +2,7 @@ import requests
 from twilio.rest import Client
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = "__YOUR_OWM_API_KEY__"
+api_key = "__YOUR_OWN_API_KEY__"
 account_sid = "__YOUR_TWILIO_ACCOUNT_ID__"
 auth_token = "__YOUR_TWILIO_AUTH_TOKEN__"
 
